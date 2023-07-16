e63ffdfd6f32e24477ed49d5bea7714015a71d3b
Zecyel <zecyel@163.com>
2023-07-16 10:40:47 +0800

    Upd: Added AppBar.

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,13 @@
 <template>
-  <router-view />
+  <!-- <router-view /> -->
+  <v-app>
+    <app-bar />
+
+    <!-- <foot-bar /> -->
+  </v-app>
 </template>
 
 <script lang="ts" setup>
-  //
+  import AppBar from '@/layouts/main/AppBar.vue'
+  import FootBar from '@/layouts/main/FootBar.vue'
 </script>
---
+++ src/layouts/main/FootBar.vue
@@ -0,0 +1,7 @@
+<template>
+    <v-footer>Hello</v-footer>
+</template>
+
+<script lang="ts" setup>
+  //
+</script>
