f78801d6aaa2e134639968798ed75f4e59e61008
Zecyel <zecyel@163.com>
2023-08-02 13:46:38 +0800

    Upd: 实现了顶部导航栏的用户登录状态显示

--- src/App.vue
+++ src/App.vue
@@ -6,11 +6,11 @@
       <router-view />
     </v-main>
 
-    <foot-bar />
+    <!-- <foot-bar /> -->
   </v-app>
 </template>
 
 <script lang="ts" setup>
   import AppBar from '@/layouts/main/AppBar.vue'
-  import FootBar from '@/layouts/main/FootBar.vue'
+  // import FootBar from '@/layouts/main/FootBar.vue'
 </script>
--- src/layouts/main/AppBar.vue
+++ src/layouts/main/AppBar.vue
@@ -7,15 +7,32 @@
     <v-app-bar-title>Filkept</v-app-bar-title>
 
     <template v-slot:append>
-      <v-btn icon="mdi-heart"></v-btn>
-  
-      <v-btn icon="mdi-magnify"></v-btn>
-  
-      <v-btn icon="mdi-dots-vertical"></v-btn>
+      <template v-if="logined()">
+        <v-btn
+          variant="text"
+        >Hello, {{ username() }}</v-btn>
+      </template>
+      <template v-else>
+        <v-btn
+          variant="text"
+        >Login</v-btn>
+        <v-btn
+          variant="text"
+        >Register</v-btn>
+      </template>
     </template>
   </v-app-bar>
 </template>
 
 <script lang="ts" setup>
-  //
+import { useUserStore } from '@/store/user';
+
+function logined(): boolean {
+    return useUserStore().token_exist
+}
+
+function username(): string {
+  return useUserStore().username
+}
+
 </script>
